ac696672d8a43577de7e867cebe41ae9ea911953
Vivek Singhal <viveksinghal0003@gmail.com>
2022-10-16 15:44:42 +0530

    fix: added password field in user

--- backend/models/user.js
+++ backend/models/user.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = mongoose.Schema({
     email: { type: String, unique: true, required: true },
     name: { type: String, required: true },
+    password: { type: String, required: true },
     orders: [
         {
             type: mongoose.Schema.Types.ObjectId,
